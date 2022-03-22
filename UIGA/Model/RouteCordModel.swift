f2d537413f48856462c405ba6c0b2087fb455a0c
NikSi <terydax30@gmail.com>
2022-03-22 03:08:42 -0700

    impementingRoute

---
+++ UIGA/Model/RouteCordModel.swift
@@ -0,0 +1,13 @@
+//
+//  RouteCordModel.swift
+//  UIGA
+//
+//  Created by Nikita Sibirtsev on 22/03/2022.
+//
+
+import Foundation
+
+struct RouteCordModel {
+    var route = [String : (Double, Double)]()
+    var name: String = ""
+}
